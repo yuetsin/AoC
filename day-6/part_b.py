aaf673bd2ad08149f808487a960d752ee81eae34
yuetsin <akaza_akari@sjtu.edu.cn>
2020-12-06 14:30:39 +0800

    [add] Day #6 Questionnaire stuff

--- day-6/part_b.py
+++ day-6/part_b.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 def get_input() -> list[str]:
     with open('./input', 'r') as f:
         return [v for v in [v.strip() for v in f.readlines()] if v]
@@ -21,3 +20,13 @@ def get_input_by_chunks() -> list[list[str]]:
         else:
             lists.append(list())
     return lists
+
+
+yes_questions_count = 0
+for chunk in get_input_by_chunks():
+    yes_questions = set('abcdefghijklmnopqrstuvwxyz')
+    for line in chunk:
+        yes_questions = yes_questions.intersection(set(line))
+    yes_questions_count += len(yes_questions)
+
+print(yes_questions_count)
